36f6ec7cb8a5d10119fdc368c6cee9d76fb0eed6
Emirhan Furkan Sakal <77203035+emirsakal@users.noreply.github.com>
2022-10-17 09:40:59 +0300

    adding ui buttons

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    
+
     public void PlayGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
