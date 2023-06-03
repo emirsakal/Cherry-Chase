371a141d7b86f552c13700813b7b088259378a86
Emirhan Furkan Sakal <emirhanfurkansakal@hotmail.com>
2023-06-03 19:27:36 +0300

    volume changer

--- Assets/Scripts/soundManager.cs
+++ Assets/Scripts/soundManager.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class soundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Slider volumeSlider;
     void Start()
     {
-        
+        if(!PlayerPrefs.HasKey("musicVolume")) {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        } else {
+            Load();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void ChangeVolume() {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    private void Load() {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void Save() {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);   
     }
 }
