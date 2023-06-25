f31342e1878b85ffbc18c20756e8657ea2bc251f
Emirhan Furkan Sakal <emirhanfurkansakal@hotmail.com>
2023-06-25 04:38:35 +0300

    star rating in levels

--- Assets/Scripts/itemCollector.cs
+++ Assets/Scripts/itemCollector.cs
@@ -2,21 +2,105 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class itemCollector : MonoBehaviour
 {
     private int cherries = 0;
+    public int[] maxCherries;
+    private int n;
 
     [SerializeField] private Text cherriesText;
+    [SerializeField] private Text maxCherriesText;
 
     [SerializeField] private AudioSource collectionSoundEffect;
+
+    /* void Start() {
+        PlayerPrefs.SetInt("level1Cherries",0);
+        PlayerPrefs.SetInt("level2Cherries",0);
+        PlayerPrefs.SetInt("level3Cherries",0);
+        PlayerPrefs.SetInt("level4Cherries",0);
+        PlayerPrefs.SetInt("level5Cherries",0);
+        PlayerPrefs.SetInt("level6Cherries",0);
+        PlayerPrefs.SetInt("level7Cherries",0);
+        PlayerPrefs.SetInt("level8Cherries",0);
+        PlayerPrefs.SetInt("level9Cherries",0);
+        PlayerPrefs.SetInt("level10Cherries",0);
+    } */
+
+    void Update() {
+        
+        switch(SceneManager.GetActiveScene().name) {
+            case "Level1":
+                n = 0;
+                if(cherries == maxCherries[0]){
+                    PlayerPrefs.SetInt("level1Cherries", 1);
+                }
+                break;
+            case "Level2":
+                n = 1;
+                if(cherries == maxCherries[1]){
+                    PlayerPrefs.SetInt("level2Cherries", 1);
+                }
+                break;
+            case "Level3":
+                n = 2;
+                if(cherries == maxCherries[2]){
+                    PlayerPrefs.SetInt("level3Cherries", 1);
+                }
+                break;
+            case "Level4":
+                n = 3;
+                if(cherries == maxCherries[3]){
+                    PlayerPrefs.SetInt("level4Cherries", 1);
+                }
+                break;
+            case "Level5":
+                n = 4;
+                if(cherries == maxCherries[4]){
+                    PlayerPrefs.SetInt("level5Cherries", 1);
+                }
+                break;
+            case "Level6":
+                n = 5;
+                if(cherries == maxCherries[5]){
+                    PlayerPrefs.SetInt("level6Cherries", 1);
+                }
+                break;
+            case "Level7":
+                n = 6;
+                if(cherries == maxCherries[6]){
+                    PlayerPrefs.SetInt("level7Cherries", 1);
+                }
+                break;
+            case "Level8":
+                n = 7;
+                if(cherries == maxCherries[7]){
+                    PlayerPrefs.SetInt("level8Cherries", 1);
+                }
+                break;
+            case "Level9":
+                n = 8;
+                if(cherries == maxCherries[8]){
+                    PlayerPrefs.SetInt("level9Cherries", 1);
+                }
+                break;
+            case "Level10":
+                n = 9;
+                if(cherries == maxCherries[9]){
+                    PlayerPrefs.SetInt("level10Cherries", 1);
+                }
+                break;
+        }
+
+    }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.CompareTag("Cherry")) {
             collectionSoundEffect.Play();
             Destroy(collision.gameObject);
             cherries ++;
-            cherriesText.text = "" + cherries;
+            cherriesText.text = cherries + "/" + maxCherries[n];
         }
     }
 }
--- Assets/Scripts/levelSelection.cs
+++ Assets/Scripts/levelSelection.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class levelSelection : MonoBehaviour
 {
     public Button[] lvlButtons;
+    public GameObject[] stars;
+    public GameObject[] emptyStars;
 
     void Start()
     {
@@ -17,6 +19,89 @@ public class levelSelection : MonoBehaviour
                 lvlButtons[i].interactable = false;
             }
         }
+        
+        if (PlayerPrefs.GetInt("level1Cherries") == 1) {
+            stars[0].SetActive(true);
+            emptyStars[0].SetActive(false);
+        } else {
+            stars[0].SetActive(false);
+            emptyStars[0].SetActive(true);
+        }
+
+        if (PlayerPrefs.GetInt("level2Cherries") == 1) {
+            stars[1].SetActive(true);
+            emptyStars[1].SetActive(false);
+        } else {
+            stars[1].SetActive(false);
+            emptyStars[1].SetActive(true);
+        }
+
+        if (PlayerPrefs.GetInt("level3Cherries") == 1) {
+            stars[2].SetActive(true);
+            emptyStars[2].SetActive(false);
+        } else {
+            stars[2].SetActive(false);
+            emptyStars[2].SetActive(true);
+        }
+
+        if (PlayerPrefs.GetInt("level4Cherries") == 1) {
+            stars[3].SetActive(true);
+            emptyStars[3].SetActive(false);
+        } else {
+            stars[3].SetActive(false);
+            emptyStars[3].SetActive(true);
+        }
+
+        if (PlayerPrefs.GetInt("level5Cherries") == 1) {
+            stars[4].SetActive(true);
+            emptyStars[4].SetActive(false);
+        } else {
+            stars[4].SetActive(false);
+            emptyStars[4].SetActive(true);
+        }
+
+        if (PlayerPrefs.GetInt("level6Cherries") == 1) {
+            stars[5].SetActive(true);
+            emptyStars[5].SetActive(false);
+        } else {
+            stars[5].SetActive(false);
+            emptyStars[5].SetActive(true);
+        }
+
+        if (PlayerPrefs.GetInt("level7Cherries") == 1) {
+            stars[6].SetActive(true);
+            emptyStars[6].SetActive(false);
+        } else {
+            stars[6].SetActive(false);
+            emptyStars[6].SetActive(true);
+        }
+
+        if (PlayerPrefs.GetInt("level8Cherries") == 1) {
+            stars[7].SetActive(true);
+            emptyStars[7].SetActive(false);
+        } else {
+            stars[7].SetActive(false);
+            emptyStars[7].SetActive(true);
+        }
+
+        if (PlayerPrefs.GetInt("level9Cherries") == 1) {
+            stars[8].SetActive(true);
+            emptyStars[8].SetActive(false);
+        } else {
+            stars[8].SetActive(false);
+            emptyStars[8].SetActive(true);
+        }
+
+        if (PlayerPrefs.GetInt("level10Cherries") == 1) {
+            stars[9].SetActive(true);
+            emptyStars[9].SetActive(false);
+        } else {
+            stars[9].SetActive(false);
+            emptyStars[9].SetActive(true);
+        }
+
+        
+        
     }
 
 }
