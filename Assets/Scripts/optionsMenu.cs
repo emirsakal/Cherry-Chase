d09d90c2b3cada22c200308fd65a0034e474cece
Emirhan Furkan Sakal <emirhanfurkansakal@hotmail.com>
2023-07-13 14:58:25 +0300

    fullscreen and resolution

---
+++ Assets/Scripts/optionsMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class optionsMenu : MonoBehaviour
+{   
+    public Dropdown resolutionDropDown;
+    Resolution[] resolutions;
+
+    void Start() {
+        resolutions = Screen.resolutions;
+
+        resolutionDropDown.ClearOptions();
+
+        List<string> options = new List<string>(); 
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++){
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height) {
+                currentResolutionIndex = i;
+            }
+        } 
+
+        resolutionDropDown.AddOptions(options);
+        resolutionDropDown.value = currentResolutionIndex;
+        resolutionDropDown.RefreshShownValue();
+    }
+
+    public void SetFullScreen(bool isFullscreen) {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution (int resolutionIndex) {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution (resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetTrailEffect (bool isTrailEffect) {
+        if(isTrailEffect){
+            PlayerPrefs.SetInt("isTrailEffectOn", 1);   
+        } else {
+            PlayerPrefs.SetInt("isTrailEffectOn", 0);
+        }
+    }
+}
