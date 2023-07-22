5310039ef8c3754e590a6617c6d7374bde64e60a
Emirhan Furkan Sakal <emirhanfurkansakal@hotmail.com>
2023-07-23 02:43:12 +0300

    adding turkish language

--- Assets/Scripts/itemCollector.cs
+++ Assets/Scripts/itemCollector.cs
@@ -10,6 +10,7 @@ public class itemCollector : MonoBehaviour
 
     public int[] maxCherries;
     private int n;
+    private bool isLevelFinished = false;
 
     [SerializeField] private Text cherriesText;
 
@@ -23,62 +24,72 @@ public class itemCollector : MonoBehaviour
         switch(SceneManager.GetActiveScene().name) {
             case "Level1":
                 n = 0;
-                if(cherries == maxCherries[0]){
+                if(cherries == maxCherries[0] && isLevelFinished){
                     PlayerPrefs.SetInt("level1Cherries", 1);
+                    isLevelFinished = false;
                 }
                 break;
             case "Level2":
                 n = 1;
-                if(cherries == maxCherries[1]){
+                if(cherries == maxCherries[1] && isLevelFinished){
                     PlayerPrefs.SetInt("level2Cherries", 1);
+                    isLevelFinished = false;
                 }
                 break;
             case "Level3":
                 n = 2;
-                if(cherries == maxCherries[2]){
+                if(cherries == maxCherries[2] && isLevelFinished){
                     PlayerPrefs.SetInt("level3Cherries", 1);
+                    isLevelFinished = false;
                 }
                 break;
             case "Level4":
                 n = 3;
-                if(cherries == maxCherries[3]){
+                if(cherries == maxCherries[3] && isLevelFinished){
                     PlayerPrefs.SetInt("level4Cherries", 1);
+                    isLevelFinished = false;
                 }
                 break;
             case "Level5":
                 n = 4;
-                if(cherries == maxCherries[4]){
+                if(cherries == maxCherries[4] && isLevelFinished){
                     PlayerPrefs.SetInt("level5Cherries", 1);
+                    isLevelFinished = false;
                 }
                 break;
             case "Level6":
                 n = 5;
-                if(cherries == maxCherries[5]){
+                if(cherries == maxCherries[5] && isLevelFinished){
                     PlayerPrefs.SetInt("level6Cherries", 1);
+                    isLevelFinished = false;
                 }
                 break;
             case "Level7":
                 n = 6;
-                if(cherries == maxCherries[6]){
+                if(cherries == maxCherries[6] && isLevelFinished){
                     PlayerPrefs.SetInt("level7Cherries", 1);
+                    isLevelFinished = false;
                 }
                 break;
             case "Level8":
                 n = 7;
-                if(cherries == maxCherries[7]){
+                if(cherries == maxCherries[7] && isLevelFinished){
                     PlayerPrefs.SetInt("level8Cherries", 1);
+                    isLevelFinished = false;
                 }
                 break;
             case "Level9":
                 n = 8;
-                if(cherries == maxCherries[8]){
+                if(cherries == maxCherries[8] && isLevelFinished){
                     PlayerPrefs.SetInt("level9Cherries", 1);
+                    isLevelFinished = false;
                 }
                 break;
             case "Level10":
                 n = 9;
-                if(cherries == maxCherries[9]){
+                if(cherries == maxCherries[9] && isLevelFinished){
                     PlayerPrefs.SetInt("level10Cherries", 1);
+                    isLevelFinished = false;
                 }
                 break;
         }
@@ -91,6 +102,9 @@ public class itemCollector : MonoBehaviour
             Destroy(collision.gameObject);
             cherries ++;
             cherriesText.text = cherries + "/" + maxCherries[n];
+        }
+        if (collision.gameObject.CompareTag("Finished")) {
+            isLevelFinished = true;
         }
     }
 }
